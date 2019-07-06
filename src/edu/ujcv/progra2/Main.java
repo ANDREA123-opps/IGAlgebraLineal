e129c0b092db8d8e4a4c6f630906521914482d32
ANDREA123-opps <andrea.avila@ujcv.edu.hn>
2019-07-06 17:16:55 -0600

    Creacion de Matrizes R2

--- src/edu/ujcv/progra2/Main.java
+++ src/edu/ujcv/progra2/Main.java
@@ -1,8 +1,11 @@
 package edu.ujcv.progra2;
 
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
     }
 }
+
--- src/edu/ujcv/progra2/matriz/IGMatR2.java
+++ src/edu/ujcv/progra2/matriz/IGMatR2.java
@@ -2,44 +2,106 @@ package edu.ujcv.progra2.matriz;
 
 import edu.ujcv.progra2.vector.IGVecR2;
 
+import javax.swing.*;
+
 public class IGMatR2 {
     // TODO: implementar.
+private int filas;
+private int columnas;
+private double matriz[][];
+    private IGMatR2 mat;
+    private int num;
+    double x1,y1;
+    double x2,y2;
+    IGVecR2 f1;
+    IGVecR2 f2;
 
-    //double x1,y1;
-    //double x2,y2;
-
-
-    // double[][] m;
-
-    //IGVecR2 f1;
-    //IGVecR2 f2;
-
-    //IGVecR2 col1;
-    //IGVecR2 col2;
-
-
-
-//    public IGMatR2 mul(IGMatR2 b){
-//
-//    }
-//
+    IGVecR2 col1;
+    IGVecR2 col2;
 
 
-//    public IGVecR2 colXMat(IGVecR2 col){
-//
-//    }
+    public int getColumnas() {
+    return columnas;
+}
 
-//    public IGMatR2 suma (IGMatR2 b){
-//
-//    }
+public void setColumnas(int columnas){
+    this.columnas = columnas;
+}
 
-//    public IGMatR2 escalarXmat(double escalar){
-//
-//    }
+public double[][] getMatriz() {
+    return matriz;
+}
 
-//    public static IGVecR2 rotacion (double angulo, IGVecR2 vector){
-//
-//    }
+public void setMatriz(double[][] matriz){
+    this.matriz = matriz;
+}
+
+public int getFilas(){
+    return filas;
+}
+
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+
+    //Constructor
+    public IGMatR2(int filas, int columnas){
+    this.filas = filas;
+    this.columnas = columnas;
+    matriz = new double[filas][columnas];
+    }
+
+    //Pedir datos
+    public void datos(){
+    int i,j;
+      for (i = 0; i<filas;i++){
+        for (j = 0; j<columnas; j++){
+            matriz[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un valor"));
+        }
+      }
+    }
 
 
+
+
+
+   public IGMatR2 mul(IGMatR2 b){
+        IGMatR2 matC = new IGMatR2(filas,mat.columnas);
+        for (int i = 0; i<filas; i++){
+            for (int j = 0; j<mat.columnas; j++){
+                int sum = 0;
+                for (double k = 0; k<columnas; k++){
+                    sum = matriz[i][j] * mat.matriz[k][j];
+                }
+                matC.matriz[i][j] = sum;
+            }
+        }
+        return matC;
+    }
+
+    public IGVecR2 colXMat(IGVecR2 col){
+
+   }
+
+      public IGMatR2 suma (IGMatR2 b) {
+       IGMatR2 resultado = new IGMatR2(filas, columnas);
+                for (int i = 0; i<filas; i++){
+                    for (int j = 0; j<columnas; j++){
+                        resultado.matriz[i][j] = matriz[i][j] + mat.matriz[i][j];
+                    }
+                }
+return resultado;
+}
+
+    public IGMatR2 escalarXmat(double escalar){
+
+    }
+
+    public static IGVecR2 rotacion (double angulo, IGVecR2 vector){
+
+    }
+
+
+
+      }
 }
